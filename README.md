6027c01444a9538339c3c659cb5e8ceb2287002e
alexm <aleksandar.muradyan@zeetta.com>
2018-09-28 10:53:45 +0100

    more API GET/POST endpoints

--- README.md
+++ README.md
@@ -10,7 +10,23 @@ virtualenv venv
 source venv/bin/activate
 pip install flask
 
-API URI:
+usage:
+
+python carrier_inventory.py --request_status <completed/failed>
+if run with 'carrier_inventory.py --request_status completed' -> will return a completed request status
+if run with 'carrier_inventory.py --request_status failed' -> will return a failed request status
+
+API URIs:
+
+GET methods:
 http://127.0.0.1:8000/locations
+http://127.0.0.1:8000/products
 http://127.0.0.1:8000/ports
-http://127.0.0.1:8000/port_spec
+http://127.0.0.1:8000/prices
+http://127.0.0.1:8000/requests
+
+POST methods:
+http://127.0.0.1:8000/login
+http://127.0.0.1:8000/locations_post
+http://127.0.0.1:8000/ports_post
+http://127.0.0.1:8000/connections_post
--- carrier_inventory.py
+++ carrier_inventory.py
@@ -1,29 +1,105 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 import os
+import argparse
 
 app = Flask(__name__)
-
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-@app.route('/locations', methods = ['GET'])
+
+def parseArguments():
+    # Create argument parser
+    #parser = argparse.ArgumentParser()
+    parser.add_argument("--RequestStatus", help="completed/failed", type=str)
+    args = parser.parse_args()
+    return args
+
+
+@app.route('/locations', methods=['GET'])
 def locations():
     with open(os.path.join(basedir, 'locations_data.json'), 'r') as f:
         return f.read()
 
-@app.route('/ports', methods = ['GET'])
-def ports():
+
+@app.route('/products', methods=['GET'])
+def products():
     with open(os.path.join(basedir, 'ports_data.json'), 'r') as f:
         return f.read()
 
-@app.route('/port_spec', methods = ['GET'])
-def port_spec():
+
+@app.route('/prices', methods=['GET'])
+def prices():
+    with open(os.path.join(basedir, 'ports_pricing_data.json'), 'r') as f:
+        return f.read()
+
+
+@app.route('/ports', methods=['GET'])
+def ports():
     with open(os.path.join(basedir, 'port_spec_data.json'), 'r') as f:
         return f.read()
 
+
+@app.route('/requests', methods=['GET'])
+def requests():
+    if args.request_status == "completed":
+        with open(os.path.join(basedir, 'requests_status_completed.json'), 'r') as f:
+            return f.read()
+    else:
+        with open(os.path.join(basedir, 'requests_status_failed.json'), 'r') as f:
+            return f.read()
+
+
+@app.route('/login', methods=['POST'])
+def login_post():
+    if request.method == 'POST':
+        # you can use <user_id>, which is a str but could
+        # changed to be int or whatever you want, along
+        # with your lxml knowl.edge to make the required
+        # changes
+        return request.json(os.path.join(basedir, 'login_post_data.json'))
+    # else:
+    #     with open(os.path.join(basedir, 'login_post_data.json'), 'r') as f:
+    #         return f.read()
+
+
+@app.route('/locations_post', methods=['POST'])
+def locations_post():
+    if request.method == 'POST':
+        return request.json(os.path.join(basedir, 'locations_post_data.json'))
+
+
+@app.route('/ports_post', methods=['POST'])
+def ports_post():
+    if request.method == 'POST':
+        return request.json(os.path.join(basedir, 'ports_post_data.json'))
+    # return jsonify(
+    #     {
+    #         "price_id": "89",
+    #         "location_id": "543",
+    #         "port_name": "LexCorp Port"
+    #     })
+
+
+@app.route('/connections_post', methods=['POST'])
+def connections_post():
+    if request.method == 'POST':
+        return request.json(os.path.join(basedir, 'connections_post_data.json'))
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
 if __name__ == "__main__":
-    port = 8000
-    # Open a web browser pointing at the app.
-    os.system("open http://localhost:{0}/".format(port))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--request_status", help="completed/failed", type=str)
+    args = parser.parse_args()
+    if args.request_status:
+        print(args.request_status)
+    print("You are running the script with arguments: ")
+    for a in args.__dict__:
+        print(str(a) + ": " + str(args.__dict__[a]))
+
+
     # Set up the development server on port 8000.
-    app.debug = True
-    app.run(port=port)
+    app.run(host='0.0.0.0', port=8000, debug=True)
